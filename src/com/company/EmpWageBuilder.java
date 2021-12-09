4f7d8ccd41a0a811817b8698cd1953df705163b1
Srinath S <srinath@Srinaths-Air.domain.name>
2021-12-09 12:46:57 +0530

    Added to UC3

--- src/com/company/EmpWageBuilder.java
+++ src/com/company/EmpWageBuilder.java
@@ -4,17 +4,26 @@ public class EmpWageBuilder {
 
     public static void main(String[] args) {
 
-        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
+        int EMP_RATE_PER_HOUR = 20;
 
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        int empHrs = 0;
+        int empWage = 0;
 
-        if (empCheck == IS_FULL_TIME)
+        double empCheck = Math.floor(Math.random() * 10) % 3;
 
-            System.out.println("Employee is Present");
+        if (empCheck == IS_PART_TIME)
+            empHrs = 4;
+
+        else if (empCheck == IS_FULL_TIME)
+            empHrs = 8;
 
         else
+            empHrs = 0;
 
-            System.out.println("Employee is Absent");
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage: " + empWage);
+
     }
-
 }
