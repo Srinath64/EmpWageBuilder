b277cadc70e23c5d40676696a241e7ce04bba71a
Srinath S <srinath@Srinaths-Air.domain.name>
2021-12-09 12:50:04 +0530

    Added to UC5

--- src/com/company/EmpWageBuilder.java
+++ src/com/company/EmpWageBuilder.java
@@ -5,6 +5,8 @@ public class EmpWageBuilder {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 2;
+
 
 
     public static void main(String[] args) {
@@ -12,27 +14,36 @@ public class EmpWageBuilder {
 
         int empHrs = 0;
         int empWage = 0;
+        int totalEmpWage = 0;
 
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        for (int day= 0; day < NUM_OF_WORKING_DAYS; day++) {
 
-        switch (empCheck) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
+            switch (empCheck) {
 
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
 
-            default:
-                empHrs = 0;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+
+                default:
+                    empHrs = 0;
+
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage += empWage;
+            System.out.println("Emp Wage: " + empWage);
 
         }
 
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Emp Wage: " + empWage);
+        System.out.println("Total EMp Wage: " + totalEmpWage);
 
     }
 
 }
+
