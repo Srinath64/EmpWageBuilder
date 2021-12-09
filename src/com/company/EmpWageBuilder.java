000eb3fd41729ac523afd1ebe368eb5a3dcbb0bc
Srinath S <srinath@Srinaths-Air.domain.name>
2021-12-09 12:50:52 +0530

    Added to UC6

--- src/com/company/EmpWageBuilder.java
+++ src/com/company/EmpWageBuilder.java
@@ -6,6 +6,7 @@ public class EmpWageBuilder {
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_OF_WORKING_DAYS = 2;
+    public static final int MAX_HOURS_IN_MONTH = 10;
 
 
 
@@ -13,11 +14,11 @@ public class EmpWageBuilder {
 
 
         int empHrs = 0;
-        int empWage = 0;
-        int totalEmpWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
 
-        for (int day= 0; day < NUM_OF_WORKING_DAYS; day++) {
-
+        while (totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays <NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
             switch (empCheck) {
@@ -35,15 +36,16 @@ public class EmpWageBuilder {
 
             }
 
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            System.out.println("Emp Wage: " + empWage);
+            totalEmpHrs += empHrs;
+            System.out.println(" Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
 
         }
-
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
         System.out.println("Total EMp Wage: " + totalEmpWage);
 
     }
 
+
 }
+
 
