5069289c496c96576a7c1d6fc26dbf7b4f2279b7
Srinath S <srinath@Srinaths-Air.domain.name>
2021-12-09 12:48:52 +0530

    Added to UC4

--- src/com/company/EmpWageBuilder.java
+++ src/com/company/EmpWageBuilder.java
@@ -2,28 +2,37 @@ package com.company;
 
 public class EmpWageBuilder {
 
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+
+
     public static void main(String[] args) {
 
-        int IS_PART_TIME = 1;
-        int IS_FULL_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
 
         int empHrs = 0;
         int empWage = 0;
 
-        double empCheck = Math.floor(Math.random() * 10) % 3;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-        if (empCheck == IS_PART_TIME)
-            empHrs = 4;
+        switch (empCheck) {
 
-        else if (empCheck == IS_FULL_TIME)
-            empHrs = 8;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
 
-        else
-            empHrs = 0;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+
+            default:
+                empHrs = 0;
+
+        }
 
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Emp Wage: " + empWage);
 
     }
+
 }
